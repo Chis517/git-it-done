ce4330bfce632cd1d1016dc9fda1ac926782e5e6
Chice <cko517@hotmail.com>
2021-11-29 19:01:02 -0500

    Request logic

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,14 @@
-var getUserRepos = function() {
-  fetch("https://api.github.com/users/Chis517/repos");
+var getUserRepos = function(user) {
+  // format the GitHub API URL
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  // make a request to the URL
+  fetch(apiUrl).then(function(response) {
+    console.log(response);
+    response.json().then(function(data) {
+      console.log(data);
+    });
+  });
 };
 
-getUserRepos();
+getUserRepos("Chis517");
